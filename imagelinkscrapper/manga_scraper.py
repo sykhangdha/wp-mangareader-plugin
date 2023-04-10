6e53032368264c9d89df7a1a592ded5baeae0834
Sykhang Ha <31758072+sykhangdha@users.noreply.github.com>
2023-04-10 05:56:04 -0700

    Update manga_scraper.py
    Added code to detect if scrapper crashes and will save .txt file from last known chapter scrapped. Also added rule to wait 2 seconds before scrolling down the page and increased wait time to next chapter from 7 seconds to 8 seconds. You can change this by editing time.sleep(8)

--- imagelinkscrapper/manga_scraper.py
+++ imagelinkscrapper/manga_scraper.py
@@ -5,8 +5,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
 import time
-import datetime  # Added datetime module for timestamp
+import datetime
 
 # Prompt user for input
 start_chapter_url = input("Enter the starting chapter URL: ")
@@ -16,7 +17,7 @@ num_chapters = int(input("Enter the number of chapters to scrape: "))
 start_chapter_num = int(start_chapter_url.split("chapter-")[1])
 
 # Set up Chrome driver
-chrome_service = ChromeService(executable_path="path/to/chromedriver.exe") # Update with path to your chromedriver executable
+chrome_service = ChromeService(executable_path="path/to/chromedriver.exe")
 driver = webdriver.Chrome(service=chrome_service)
 
 # Navigate to starting chapter URL
@@ -34,53 +35,63 @@ def scroll_down():
     actions = ActionChains(driver)
     actions.send_keys(Keys.END)  # Scroll to the bottom of the page
     actions.perform()
-    time.sleep(0.5)  # Adjust delay as needed to scroll faster
+    time.sleep(2)  # Wait 2 seconds before scrolling
 
-# Loop through chapters and scrape image links
-for i in range(num_chapters):
-    chapter_num = start_chapter_num + i
-    chapter_url = start_chapter_url.replace("chapter-{}".format(start_chapter_num), "chapter-{}".format(chapter_num))
-    driver.get(chapter_url)
-    print("Scraping images from chapter: ", chapter_url)
+try:
+    # Loop through chapters and scrape image links
+    for i in range(num_chapters):
+        chapter_num = start_chapter_num + i
+        chapter_url = start_chapter_url.replace("chapter-{}".format(start_chapter_num), "chapter-{}".format(chapter_num))
+        driver.get(chapter_url)
+        print("Scraping images from chapter: ", chapter_url)
 
-    try:
-        # Wait for the lazy-loading of images
-        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='vung-doc']/img")))
-    except TimeoutException:
-        print("No images found in chapter: ", chapter_url)
-        continue
+        try:
+            # Wait for the lazy-loading of images
+            WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='vung-doc']/img")))
+        except TimeoutException:
+            print("No images found in chapter: ", chapter_url)
+            continue
 
-    # Scroll down the page to trigger lazy-loading of images
-    while True:
-        prev_height = driver.execute_script("return document.body.scrollHeight")
-        scroll_down()
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == prev_height:
-            break
+        # Scroll down the page to trigger lazy-loading of images
+        while True:
+            prev_height = driver.execute_script("return document.body.scrollHeight")
+            scroll_down()
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == prev_height:
+                break
 
-    # Wait for images to load
-    time.sleep(5)  # Increased delay to allow images to load properly
+        # Wait for images to load
+        time.sleep(5)
 
-    # Scraping image links
-    img_elements = driver.find_elements(By.XPATH, "//div[@class='vung-doc']/img")
-    image_links = []
-    for img_element in img_elements:
-        img_url = img_element.get_attribute("src")
-        if img_url:
-            print(img_url)
-            image_links.append(img_url)
-
-    # Store chapter heading and image links in dictionary
-    chapter_heading = driver.find_element(By.XPATH, "//h1").text
-    chapter_data[chapter_heading] = image_links
-
-    # Delay for 7 seconds before navigating to the next chapter
-    time.sleep(7)
-
-# Save chapter headings and image links to a single .txt file with timestamp appended to the file name
-with open("chapter_image_links_{}.txt".format(timestamp), "w") as file:
-    for chapter_heading, image_links in chapter_data.items():
-        file.write("Chapter {}\n".format(chapter_heading))
-        for img_url in image_links:
+        # Scraping image links
+        img_elements = driver.find_elements(By.XPATH, "//div[@class='vung-doc']/img")
+        image_links = []
+        for img_element in img_elements:
+            img_url = img_element.get_attribute("src")
             if img_url:
-                file.write("{}\n".format(img_url))
+                print(img_url)
+                image_links.append(img_url)
+
+        # Store chapter heading and image links in dictionary
+        chapter_heading = driver.find_element(By.XPATH, "//h1").text
+        chapter_data[chapter_heading] = image_links
+
+        # Delay for 8 seconds before navigating to the next chapter
+        time.sleep(8)
+
+except Exception as e:
+    print("Error occurred: ", e)
+
+finally:
+    # Save chapter headings and image links to a single .txt file with timestamp appended to
+    output_file = "manga_data_{}.txt".format(timestamp)
+    with open(output_file, "w", encoding="utf-8") as f:
+        for chapter_heading, image_links in chapter_data.items():
+            f.write("Chapter: {}\n".format(chapter_heading))
+            for i, image_link in enumerate(image_links):
+                f.write("Image {}: {}\n".format(i + 1, image_link))
+            f.write("\n")
+    print("Chapter data saved to file: ", output_file)
+
+    # Close the webdriver
+    driver.quit()
