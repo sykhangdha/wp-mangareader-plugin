ceb30950338adf426e6fbf468c620db3d8331605
Sykhang Ha <31758072+sykhangdha@users.noreply.github.com>
2023-06-08 00:45:22 -0700

    Create single.php

---
+++ single.php
@@ -0,0 +1,4 @@
+//COPY AND PASTE THE CODE BELOW
+
+<nav class="next"><?php next_post_link('%link', '<strong>'.esc_html__('').'</strong> <span>%title</span>' , true); ?></nav>
+<nav class="previous"><?php previous_post_link('%link', '<strong>'.esc_html__('').'</strong> <span>%title</span>' , true); ?></nav>
