02a252cc89ce84a6928ed28b8907a886797bc60d
Sykhang Ha <31758072+sykhangdha@users.noreply.github.com>
2023-09-01 12:54:10 -0700

    Update single.php
    update

--- single.php
+++ single.php
@@ -1,4 +1,4 @@
-//COPY AND PASTE THE CODE BELOW
+//Example code the main thing you want to add to your next_post_link is the 'true' logic for the same category
 
 <nav class="next"><?php next_post_link('%link', '<strong>'.esc_html__('').'</strong> <span>%title</span>' , true); ?></nav>
 <nav class="previous"><?php previous_post_link('%link', '<strong>'.esc_html__('').'</strong> <span>%title</span>' , true); ?></nav>
