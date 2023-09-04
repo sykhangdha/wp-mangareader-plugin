ff6c96250b3368e516c5d015d7a20768ddaad7e1
Sykhang Ha <31758072+sykhangdha@users.noreply.github.com>
2023-09-03 17:54:41 -0700

    Update manga-reader.php
    Adds shortcode ONLY when there are image links inside the custom field

--- manga-reader.php
+++ manga-reader.php
@@ -2,11 +2,12 @@
 /**
  * Plugin Name: Manga Reader
  * Description: A manga reader plugin.
- * Version: 2.5
+ * Version: 3.0
  * Author: Ha Sky
  * Author URI: https://hasky.rf.gd
  **/
 
+// Register shortcode
 function manga_reader_shortcode($atts) {
     // Extract shortcode attributes
     extract(shortcode_atts(array(
@@ -17,11 +18,15 @@ function manga_reader_shortcode($atts) {
 
     // Check if images were provided as shortcode attribute, if not, get image links from custom field
     if (empty($images)) {
-        $images = get_post_meta(get_the_ID(), 'image_links', true);
-        // Convert image links to array
-        $images = preg_split('/\r\n|[\r\n]/', $images);
-        $images = array_map('trim', $images);
-        $images = array_filter($images);
+        $image_links = get_post_meta(get_the_ID(), 'image_links', true);
+
+        // Check if there are links in the custom field 'image_links'
+        if (!empty($image_links)) {
+            // Convert image links to array
+            $images = preg_split('/\r\n|[\r\n]/', $image_links);
+            $images = array_map('trim', $images);
+            $images = array_filter($images);
+        }
     } else {
         // Convert image links to array
         $images = preg_split('/\s*(?:,|$)\s*/', $images);
@@ -43,18 +48,6 @@ function manga_reader_shortcode($atts) {
 
     return $output;
 }
-
-// Load scripts
-function manga_reader_scripts() {
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('manga-reader-script', plugins_url('manga-reader.js', __FILE__), array('jquery'), '1.2', true);
-
-    // Pass loading image URL to JavaScript file
-    wp_localize_script( 'manga-reader-script', 'manga_data', array(
-        'loading_image' => plugins_url( '/images/loading.gif', __FILE__ )
-    ));
-}
-add_action('wp_enqueue_scripts', 'manga_reader_scripts');
 
 // Register shortcode
 function manga_reader_register_shortcode() {
@@ -112,20 +105,21 @@ function manga_reader_template($template) {
 }
 add_filter('template_include', 'manga_reader_template');
 
-// Save previous post status
-function manga_reader_save_previous_post_status($post_id, $post) {
-    update_post_meta($post_id, '_previous_post_status', $post->post_status);
-}
-add_action('wp_insert_post', 'manga_reader_save_previous_post_status', 10, 2);
+// Add shortcode on post publish or update
+function manga_reader_add_shortcode_on_publish($post_id) {
+    $post = get_post($post_id);
 
-// Add shortcode on post publish
-function manga_reader_add_shortcode_on_publish($post_id, $post) {
-    if ($post->post_type === 'post' && $post->post_status === 'publish') {
-        $content = $post->post_content;
-        if (strpos($content, '[manga_reader]') === false) {
-            $updated_content = $content . '[manga_reader]';
-            wp_update_post(array('ID' => $post_id, 'post_content' => $updated_content));
+    if ($post->post_type === 'post' && ($post->post_status === 'publish' || $post->post_status === 'draft')) {
+        $image_links = get_post_meta($post_id, 'image_links', true);
+
+        // Check if there are links in the custom field 'image_links'
+        if (!empty($image_links)) {
+            $content = $post->post_content;
+            if (strpos($content, '[manga_reader]') === false) {
+                $updated_content = $content . '[manga_reader]';
+                wp_update_post(array('ID' => $post_id, 'post_content' => $updated_content));
+            }
         }
     }
 }
-add_action('save_post', 'manga_reader_add_shortcode_on_publish', 10, 2);
+add_action('save_post', 'manga_reader_add_shortcode_on_publish', 10, 1);
