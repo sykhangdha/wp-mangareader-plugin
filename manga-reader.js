4736a61f0234642961d3c81af84685b4542ad5bf
Sykhang Ha <31758072+sykhangdha@users.noreply.github.com>
2024-04-21 20:09:22 -0700

    Update manga-reader.js
    brings back paged/list view mode functionality from previous script. Swipe functions currently do not work properly

--- manga-reader.js
+++ manga-reader.js
@@ -3,24 +3,22 @@ jQuery(document).ready(function($) {
   var mangaImages = mangaReader.find('.manga-images a.img-popup');
   var currentIndex = 0;
   var totalPages = mangaImages.length;
+  var magnificPopupInstance = null;
 
-  var scrollToTop = function() {
-    var currentImage = $(mangaImages[currentIndex]);
-    var top = currentImage.offset().top;
-    $('html, body').animate({
-      scrollTop: top
-    }, 500);
-  };
-
-  var handleEscapeKey = function(e) {
-    if (e.keyCode === 27) { // Escape key
-      $.magnificPopup.close();
+  var scrollToNextImage = function() {
+    if (currentIndex < totalPages - 1) {
+      currentIndex++;
+      var currentImage = $(mangaImages[currentIndex]);
+      var top = currentImage.offset().top;
+      $('html, body').animate({
+        scrollTop: top
+      }, 500);
     }
   };
 
   var openMagnificPopup = function(index) {
     if (index < totalPages) {
-      $.magnificPopup.open({
+      magnificPopupInstance = $.magnificPopup.open({
         items: mangaImages.toArray(),
         gallery: {
           enabled: true
@@ -34,9 +32,15 @@ jQuery(document).ready(function($) {
           },
           close: function() {
             scrollToTop();
+            $('.exit-button').hide(); // Hide exit button when popup is closed
           },
           beforeClose: function() {
-            $(document).off('keydown.magnificPopup', handleEscapeKey);
+            $(document).off('keydown.magnificPopup');
+          },
+          open: function() {
+            $('.exit-button').show(); // Show exit button when popup is opened
+            // Handle escape key separately
+            $(document).on('keydown.magnificPopup', handleEscapeKey);
           }
         },
         closeOnBgClick: false,
@@ -50,20 +54,23 @@ jQuery(document).ready(function($) {
           }
         }
       }, index);
-
-      // Add close button in Magnific Popup
-      $.magnificPopup.instance.close = function() {
-        if ($.magnificPopup.proto.close) {
-          $.magnificPopup.proto.close.call(this);
-          scrollToTop();
-        }
-      };
-
-      // Handle escape key separately
-      $(document).on('keydown.magnificPopup', handleEscapeKey);
     } else {
       // Close the gallery if trying to open beyond the last image
       $.magnificPopup.close();
+    }
+  };
+
+  var scrollToTop = function() {
+    var currentImage = $(mangaImages[currentIndex]);
+    var top = currentImage.offset().top;
+    $('html, body').animate({
+      scrollTop: top
+    }, 500);
+  };
+
+  var handleEscapeKey = function(e) {
+    if (e.keyCode === 27 && magnificPopupInstance !== null) { // Escape key
+      magnificPopupInstance.close();
     }
   };
 
@@ -72,55 +79,42 @@ jQuery(document).ready(function($) {
   };
 
   var handleSwipeEnd = function() {
-    var delta = touchEndX - touchStartX;
-
-    if (delta > 50) {
-      // Swipe right, go to the previous image
-      if (currentIndex > 0) {
-        currentIndex--;
-        openMagnificPopup(currentIndex);
-      }
-    } else if (delta < -50) {
-      // Swipe left, go to the next image
-      if (currentIndex < totalPages - 1) {
-        currentIndex++;
-        openMagnificPopup(currentIndex);
-      } else {
-        // Reached the last image, close the gallery
-        $.magnificPopup.close();
-      }
-    }
-
-    // Reset touch coordinates
-    touchStartX = null;
-    touchEndX = null;
+    // Handle swipe end event if needed
   };
 
-  // Touch event variables
-  var touchStartX = null;
-  var touchEndX = null;
+  // Function to open Magnific Popup image gallery
+  var openPagedView = function() {
+    openMagnificPopup(currentIndex);
+  };
 
+  // Add "Paged view" button
+  var pagedViewButton = $('<button/>', {
+    text: 'Switch to Paged View',
+    class: 'paged-view-button',
+    click: openPagedView
+  });
+
+  // Append button to the reader
+  mangaReader.prepend(pagedViewButton);
+
+
+  // Click event for mangaImages
   mangaImages.click(function(e) {
     e.preventDefault();
-    openMagnificPopup(currentIndex);
+    scrollToNextImage();
   });
 
-  $(document).on('touchstart', '.mfp-img', function(e) {
-    touchStartX = e.originalEvent.touches[0].pageX;
+  // Close popup when clicking outside of the image or pressing Escape key
+  $(document).on('click', function(e) {
+    if (!$(e.target).closest('.mfp-container').length && magnificPopupInstance !== null) {
+      magnificPopupInstance.close();
+    }
   });
 
-  $(document).on('touchmove', '.mfp-img', function(e) {
-    // Handle touch move event if needed
-    handleSwipeMove(e);
-  });
-
-  $(document).on('touchend', '.mfp-img', function(e) {
-    touchEndX = e.originalEvent.changedTouches[0].pageX;
-    handleSwipeEnd();
-  });
-
-  // Close button click event
-  $(document).on('click', '.mfp-close, .mfp-arrow-right, .mfp-arrow-left', function() {
-    $.magnificPopup.close();
+  // Close popup when pressing Escape key
+  $(document).on('keydown', function(e) {
+    if (e.keyCode === 27 && magnificPopupInstance !== null) { // Escape key
+      magnificPopupInstance.close();
+    }
   });
 });
